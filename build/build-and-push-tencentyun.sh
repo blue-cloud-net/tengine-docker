d3bc84132d5054ef0bd650f2d0eb2d499c1c03c0
huansky <651494458@qq.com>
2025-06-23 11:32:56 +0800

    chore:调账编译脚本

--- build/build-and-push-tencentyun.sh
+++ build/build-and-push-tencentyun.sh
@@ -17,17 +17,17 @@ for dockerfile in $dockerfiles; do
     # Cross compile the Dockerfile for multiple environments
     # platforms=("linux/amd64" "linux/arm/v7" "linux/arm64/v8");
     # platforms=("linux/amd64" "linux/arm64");
-    # platforms=("linux/amd64");
+    platforms=("linux/amd64");
     # echo "Building tengine:$version";
-    # platforms_csv=$(IFS=,; echo "${platforms[*]}");
+    platforms_csv=$(IFS=,; echo "${platforms[*]}");
 
     # Build the Docker image for the specified platforms
     # Use --platform to specify the target platforms
     # Use --push to push the image directly to the repository
-    # docker buildx build --platform "$platforms_csv" -f "$dockerfile" -t "$repository:$version" --push .;
+    docker buildx build --platform "$platforms_csv" -f "$dockerfile" -t "$repository:$version" --push .;
 
     # Build the Docker image for the default platform
-    docker build -f "$dockerfile" -t "$repository:$version" .;
+    # docker build -f "$dockerfile" -t "$repository:$version" --build-arg ALL_PROXY="http://192.168.199.199:7890" .;
     
     # Push the Docker image to the private repository
     docker push "$repository:$version";
